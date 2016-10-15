cdb888ba687e37590aca51dcb48f8a7bba973e6f
JamesVanWaza <jgacuca@gmail.com>
2016-10-15 19:36:34 -0400

    Updated the html5req page

--- html5req.php
+++ html5req.php
@@ -4,11 +4,8 @@
 <meta name="google-signin-client_id" content="537511216142-covjs8kdv2k5ab2ivajmr1qchdkjgjlr.apps.googleusercontent.com">
 <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 <head>
-    <link href="css/foundation.css" rel="stylesheet" type="text/css">
-    <link href="css/main.css" rel="stylesheet" type="text/css">
-    <link href="css/prism.css" rel="stylesheet" type="text/css">
-    <link href="bower_components/components-font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
-    <link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16">
+    <link href="css/app.css" rel="stylesheet" type="text/css">
+    <link href="node_modules/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
     <link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="images/favicon-64x64.png" sizes="64x64">
     <link rel="icon" type="image/png" href="images/favicon-128x128.png" sizes="128x128">
