49f4a540dc0e158b4f7b24af79ae70cb4b0d396d
JamesVanWaza <jgacuca@gmail.com>
2016-11-06 00:25:59 -0400

    Updated Grunt Setup

--- grunt.php
+++ grunt.php
@@ -1,11 +1,16 @@
 <?php include_once 'nav-admin.php';?>
-Install Node Package Manager and Grunt
+<div class="row">
+	<div class="small-12 medium-12 large-12 columns">
+		Install Node Package Manager and Grunt
 <code class="language-git">sudo npm install -g grunt-cli</code>
 
 Install SASS
 <code class="language-git">sudo gem install sass</code>
 
 Add 2 Files
-package.json
+To Add package.json <code class="language-git">npm init</code>
 gruntfile.js
+	</div>
+</div>
+
 <?php include_once 'footer.php';?>
