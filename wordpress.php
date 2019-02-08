cf8e59203635369a0d384aa07b0118ccf1d5800a
JamesVanWaza <info@dosbranding.com>
2019-02-08 18:42:18 -0500

    Embedded the Wordpress Youtube

--- wordpress.php
+++ wordpress.php
@@ -11,4 +11,5 @@
 				</code>
 		</div>
 	</div>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/1h8jDeYQpPY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <?php include 'footer.php';?>
