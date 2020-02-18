cca5ee58640e1aa8acc3ba2b2b068623ba2a62a6
JamesVanWaza <jgacuca@gmail.com>
2020-02-18 13:18:28 -0500

    jQuery has been added

--- footer.php
+++ footer.php
@@ -1,13 +1,12 @@
-<footer id="footer" class="medium-text-center large-text-center">
-	<?php echo "<i class='fa fa-copyright'></i> JAMESVANWAZA 2014 -" . date('Y'); ?>
-</footer>
-<script src="node_modules/jquery/dist/jquery.min.js"></script>
-<script src="node_modules/foundation-sites/dist/js/foundation.min.js"></script>
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-<script src="js/prism.js"></script>
-<script src="js/gso.js"></script>
-<script src="js/stickyfooter.js"></script>
-<script src="js/creditcard.js"></script>
-<script src="js/app.js"></script>
+	<footer id="footer" class="medium-text-center large-text-center">
+		<?php echo "<i class='far fa-copyright'></i> JAMESVANWAZA 2014 -" . date('Y'); ?>
+	</footer>
+
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
+	<script src="js/prism.js"></script>
+	<script src="js/stickyfooter.js"></script>
+	<script src="js/creditcard.js"></script>
+	<script src="js/app.js"></script>
 </body>
 </html>
