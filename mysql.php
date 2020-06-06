31eb9e84b92d863b2bfa46b2125b243f178fc71b
JamesVanWaza <jgacuca@gmail.com>
2020-06-06 14:23:35 -0400

    Changed the DatabaseName

--- mysql.php
+++ mysql.php
@@ -1,4 +1,4 @@
-<?php include_once 'nav-admin.php';?>
+<?php include_once 'nav-admin.php'; ?>
 <section class="row">
 	<h2 class="text-center">SQL TIPS AND TRICKS</h2>
 	<div class="small-12 medium-12 large-12 columns">
@@ -35,9 +35,9 @@
 <div class="row">
 	<div class="small-12 medium-6 large-6 columns">
 		<p>My SQL Resources</p>
-			<ul class="no-bullet">
-				<li><a href="http://code.tutsplus.com/articles/an-introduction-to-stored-procedures-in-mysql-5--net-17843" target="_blank">MySQL Procedures Example</a></li>
-			</ul>
+		<ul class="no-bullet">
+			<li><a href="http://code.tutsplus.com/articles/an-introduction-to-stored-procedures-in-mysql-5--net-17843" target="_blank">MySQL Procedures Example</a></li>
+		</ul>
 	</div>
 	<article class="small-12 medium-6 large-6 columns">
 		<ol>
@@ -55,7 +55,7 @@
 			</li>
 			<li>
 				Exporting a Database with a TIMESTAMP
-				<code class="language-sql">@DATABASE@-%F-%T</code> e.g <br>
+				<code class="language-sql">DATABASE-%F-%T</code> e.g <br>
 				<code class="language-sql">DatabaseName-2016-12-01 7:00</code>
 			</li>
 			<li>
@@ -65,13 +65,13 @@
 			<li>Get Size of SQL Database</li>
 			<code class="language-sql">
 				SELECT table_schema
-					<br>
-   					Round(Sum(data_length + index_length) / 1024 / 1024, 1) "DB Name", "DB Size in MB"
-   					<br>
-					FROM   information_schema.tables
-					<br>
-					GROUP  BY table_schema; </code>
+				<br>
+				Round(Sum(data_length + index_length) / 1024 / 1024, 1) "DB Name", "DB Size in MB"
+				<br>
+				FROM information_schema.tables
+				<br>
+				GROUP BY table_schema; </code>
 		</ol>
 	</article>
 </div>
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
