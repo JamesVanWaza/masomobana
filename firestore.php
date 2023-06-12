d3ef2374e98adc1c3715c05f00d7f9b20d913479
JamesVanWaza <jgacuca@gmail.com>
2023-06-12 13:19:19 -0400

    Updated the URL

--- firestore.php
+++ firestore.php
@@ -4,7 +4,7 @@
         <li><a href="https://ad.dostesting.co.ke" target="_blank">AISD</a></li>
         <li><a href="https://badwf.dostesting.co.ke" target="_blank">BADWF</a></li>
         <li><a href="https://dostesting.co.ke" target="_blank">DOSBOOTSTRAP</a></li>
-        <li><a href="https://fisstaffing.dostesting.co.ke" target="_blank">FIS STAFFING</a></li>
+        <li><a href="https://fis.dostesting.co.ke" target="_blank">FIS STAFFING</a></li>
         <li><a href="https://heart4kids.dostesting.co.ke" target="_blank">HEART 4 KIDS</a></li>
         <li><a href="https://start-sass.dostesting.co.ke" target="_blank">START SASS</a><li>
         <li><a href="https://tailwind-start.dostesting.co.ke" target="_blank">TAILWIND</a><li>
