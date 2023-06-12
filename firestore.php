802277b5865c72081e87d1015f5f1d9e441cea5f
JamesVanWaza <jgacuca@gmail.com>
2023-06-12 13:15:28 -0400

    Create firestore.php

---
+++ firestore.php
@@ -0,0 +1,13 @@
+<?php include 'nav-admin.php';?>
+    <h1>Web Projects</h1>
+    <ol>
+        <li><a href="https://ais.dostesting.co.ke" target="_blank">AIS</a></li>
+        <li><a href="https://badwf.dostesting.co.ke" target="_blank">BADWF</a></li>
+        <li><a href="https://dosbootstrap.dostesting.co.ke" target="_blank">DOSBOOTSTRAP</a></li>
+        <li><a href="https://fisstaffing.dostesting.co.ke" target="_blank">FIS STAFFING</a></li>
+        <li><a href="https://heart4kids.dostesting.co.ke" target="_blank">HEART 4 KIDS</a></li>
+        <li><a href="https://start-sass.dostesting.co.ke" target="_blank">START SASS</a><li>
+        <li><a href="https://tailwind-start.dostesting.co.ke" target="_blank">TAILWIND</a><li>
+        <li><a href="https://zeicloud.dostesting.co.ke" target="_blank">ZEICLOUD</a><li>
+    </ol>
+<?php include 'footer.php';?>
