b36049ce08db91fb962181a8cfe9906c5130cf21
JamesVanWaza <jgacuca@gmail.com>
2023-06-12 13:18:38 -0400

    Removed the dosbootstrap

--- firestore.php
+++ firestore.php
@@ -3,7 +3,7 @@
     <ol>
         <li><a href="https://ad.dostesting.co.ke" target="_blank">AISD</a></li>
         <li><a href="https://badwf.dostesting.co.ke" target="_blank">BADWF</a></li>
-        <li><a href="https://dosbootstrap.dostesting.co.ke" target="_blank">DOSBOOTSTRAP</a></li>
+        <li><a href="https://dostesting.co.ke" target="_blank">DOSBOOTSTRAP</a></li>
         <li><a href="https://fisstaffing.dostesting.co.ke" target="_blank">FIS STAFFING</a></li>
         <li><a href="https://heart4kids.dostesting.co.ke" target="_blank">HEART 4 KIDS</a></li>
         <li><a href="https://start-sass.dostesting.co.ke" target="_blank">START SASS</a><li>
