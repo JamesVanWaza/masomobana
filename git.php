2c7dd5a7e72ac2a4e01e709b2a686497add13eca
JamesVanWaza <jgacuca@gmail.com>
2023-06-12 17:21:59 -0400

    Added div.container-fluid

--- git.php
+++ git.php
@@ -1,25 +1,29 @@
 <?php include_once 'nav-admin.php';?>
-<section class="row">
-  <div class="small-12 medium-6 large-6 columns">
-    <ul class="no-bullet">
-      <li><a href="http://rogerdudler.github.io/git-guide" target="_blank">Git Guide</a></li>
-      <li><a href="http://git-tower.com/blog/git-cheat-sheet-detail" target="_blank">Git Cheat Sheet</a></li>
-    </ul>
-  </div>
-  <div class="small-12 medium-6 large-6 columns">
-    Removing a Repository Created with Git Init
-    <br>
-    <code class="language-css">rm -rf .git</code>
-  </div>
-  <div class="small-12 medium-6 large-6 columns">
-    Removing files from a Repository
-    <br>
-    <code class="language-css">git rm --cached -r</code>
-  </div>
+
+<div class="container-fluid">
+  <section class="row">
     <div class="small-12 medium-6 large-6 columns">
-    Renaming a branch
-    <br>
-    <code class="language-css">git branch -m OLDBRANCHNAME NEWBRANCHNAME</code>
-  </div>
-</section>
+      <ul class="no-bullet">
+        <li><a href="http://rogerdudler.github.io/git-guide" target="_blank">Git Guide</a></li>
+        <li><a href="http://git-tower.com/blog/git-cheat-sheet-detail" target="_blank">Git Cheat Sheet</a></li>
+      </ul>
+    </div>
+    <div class="small-12 medium-6 large-6 columns">
+      Removing a Repository Created with Git Init
+      <br>
+      <code class="language-css">rm -rf .git</code>
+    </div>
+    <div class="small-12 medium-6 large-6 columns">
+      Removing files from a Repository
+      <br>
+      <code class="language-css">git rm --cached -r</code>
+    </div>
+      <div class="small-12 medium-6 large-6 columns">
+      Renaming a branch
+      <br>
+      <code class="language-css">git branch -m OLDBRANCHNAME NEWBRANCHNAME</code>
+    </div>
+  </section>
+</div>
+
 <?php include_once 'footer.php';?>
