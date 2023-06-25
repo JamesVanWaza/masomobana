887b9569ce0f3e33407447184bc0ab37a04fe17a
JamesVanWaza <jgacuca@gmail.com>
2023-06-25 18:05:08 -0400

    Setup the Favicon

--- html5req.php
+++ html5req.php
@@ -2,6 +2,6 @@
 <html lang="en">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <head>
-    <link rel="icon" href="images/" type="image/MasomoBana-Color-Favicon@4x.png" sizes="128x128">
+    <link rel="icon" href="images/MasomoBana-Color-Favicon@4x.png" type="image/x-icon" sizes="128x128">
 </head>
 <body>
