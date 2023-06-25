37623610f287720ff9dc796738133a9573f075fe
JamesVanWaza <jgacuca@gmail.com>
2023-06-25 18:01:31 -0400

    Rewriting the HTML5

--- html5req.php
+++ html5req.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<meta name="google-signin-client_id" content="537511216142-covjs8kdv2k5ab2ivajmr1qchdkjgjlr.apps.googleusercontent.com">
-<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 <head>
     <link rel="icon" href="images/" type="image/MasomoBana-Color-Favicon@4x.png" sizes="128x128">
 </head>
