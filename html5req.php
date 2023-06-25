88244a0bbfeadff8d385000201b5d92e8f95ba34
JamesVanWaza <jgacuca@gmail.com>
2023-06-25 17:58:29 -0400

    Added Favicon

--- html5req.php
+++ html5req.php
@@ -4,12 +4,6 @@
 <meta name="google-signin-client_id" content="537511216142-covjs8kdv2k5ab2ivajmr1qchdkjgjlr.apps.googleusercontent.com">
 <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-	<link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-    <link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="images/favicon-64x64.png" sizes="64x64">
-    <link rel="icon" type="image/png" href="images/favicon-128x128.png" sizes="128x128">
-    <link rel="icon" type="image/png" href="images/favicon-256x256.png" sizes="256x256">
-    <link rel="icon" type="image/png" href="images/favicon-512x512.png" sizes="512x512">
+    <link rel="icon" href="images/" type="image/MasomoBana-Color-Favicon@4x.png" sizes="128x128">
 </head>
 <body>
