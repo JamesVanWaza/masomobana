686649981f1c0cd797d27ebf6a32e842cc4d8e46
JamesVanWaza <jgacuca@gmail.com>
2023-06-25 17:48:02 -0400

    Added prism.js

--- footer.php
+++ footer.php
@@ -7,5 +7,6 @@
 	<script src="js/prism.js"></script> -->
 	<script src="js/app.js"></script>
 	<script src="js/index.js" type="module"></script>
+	<script src="js/prism.js"></script>
 </body>
 </html>
