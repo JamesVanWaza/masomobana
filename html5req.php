fd33368b56a1c7d03cd522da1a34e9c2d1f13fc3
JamesVanWaza <jgacuca@gmail.com>
2023-09-24 17:11:05 -0400

    Changed the header to dark mode

--- html5req.php
+++ html5req.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <head>
     <link rel="icon" href="images/MasomoBana-Color-Favicon@4x.png" type="image/x-icon" sizes="128x128">
