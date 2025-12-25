e27115086079e0402fc509b3e275c94cb2d77b56
JamesVanWaza <jgacuca@gmail.com>
2025-12-25 11:38:53 -0500

    Added info on how to wrap elements in visual studio code

--- html.php
+++ html.php
@@ -13,6 +13,17 @@
 				<a href="tel:+12023456789">(202)-345-6789</a>
 			</code>
 	</div>
+
+	<div class="large-6 medium-6 small-12 columns">
+		Wrapping Elements in Visual Studio Code
+		<p>To wrap an element in Visual Studio Code:</p>
+		<ol>
+			<li>Select the element you want to wrap</li>
+			<li>Press <code>Shift + Alt + W</code> (Windows) or <code>Shift + Option + W</code> (Mac)</li>
+			<li>Type the tag name you want to wrap the element with and press <code>Enter</code></li>
+		</ol>
+
+	</div>
 </section>
 
 <?php include_once 'footer.php';?>
