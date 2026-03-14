b91198a23f5075af3705fdbf5a4412ec32dbf0f3
JamesVanWaza <jgacuca@gmail.com>
2026-03-14 17:00:10 -0400

    Added a class for the linked files

--- index.php
+++ index.php
@@ -5,42 +5,42 @@
     <div class="col-lg-3 col-md-6 col-sm-12">
       <p>DAILY WORKFLOW</p>
       <ul class="no-bullet">
-        <li><a href="https://getbootstrap.com/" target="_blank">Bootstrap 5 <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">JS <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://php.net/" target="_blank">PHP <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://dev.mysql.com/doc/refman/8.0/en/" target="_blank">MYSQL <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://getbootstrap.com/" target="_blank">Bootstrap 5 <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">JS <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://php.net/" target="_blank">PHP <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://dev.mysql.com/doc/refman/8.0/en/" target="_blank">MYSQL <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
 
     <div class="col-lg-3 col-md-6 col-sm-12">
       <p>COMPLETED WEBSITES</p>
       <ul class="no-bullet">
-        <li><a href="https://jamesvanwaza.com/" target="_blank">James Van Waza <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://jamesvanwaza.com/" target="_blank">James Van Waza <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
 
     <div class="col-lg-3 col-md-6 col-sm-12">
       <p>HTDOCS WEBSITES</p>
       <ul class="no-bullet">
-        <li><a href="https://ad.dostesting.co.ke" target="_blank">AID <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://badwf.dostesting.co.ke" target="_blank">BADWF <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://b5e.dostesting.co.ke" target="_blank">Bootstrap 5 Examples <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://dostesting.co.ke" target="_blank">Dosbootstrap <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://fis.dostesting.co.ke" target="_blank">FIS <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://heart4kids.dostesting.co.ke" target="_blank">Heart 4 Kids <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://kenafrica-bootstrap.dostesting.co.ke" target="_blank">Kenafrica Bootstrap <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://start-sass.dostesting.co.ke" target="_blank">Start SASS<i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://tailwind-start.dostesting.co.ke" target="_blank">Tailwind <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://webpack.dostesting.co.ke" target="_blank">Webpack <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://zeicloud.dostesting.co.ke" target="_blank">ZeIcloud <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://ad.dostesting.co.ke" target="_blank">AID <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://badwf.dostesting.co.ke" target="_blank">BADWF <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://b5e.dostesting.co.ke" target="_blank">Bootstrap 5 Examples <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://dostesting.co.ke" target="_blank">Dosbootstrap <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://fis.dostesting.co.ke" target="_blank">FIS <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://heart4kids.dostesting.co.ke" target="_blank">Heart 4 Kids <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://kenafrica-bootstrap.dostesting.co.ke" target="_blank">Kenafrica Bootstrap <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://start-sass.dostesting.co.ke" target="_blank">Start SASS<i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://tailwind-start.dostesting.co.ke" target="_blank">Tailwind <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://webpack.dostesting.co.ke" target="_blank">Webpack <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://zeicloud.dostesting.co.ke" target="_blank">ZeIcloud <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
 
     <div class="col-lg-3 col-md-6 col-sm-6">
       <p>MAGAZINE ARTICLES</p>
       <ul class="no-bullet">
-        <li><a href="https://alistapart.com/" target="_blank">A List Apart <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://smashingmagazine.com/" target="_blank">Smashing Magazine <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://alistapart.com/" target="_blank">A List Apart <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://smashingmagazine.com/" target="_blank">Smashing Magazine <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
   </section>
@@ -49,42 +49,42 @@
     <div class="col-lg-3 col-md-6 col-sm-6">
       <p>OTHER MAGAZINES</p>
       <ul class="no-bullet">
-        <li><a href="https://thenextweb.com/" target="_blank">The Next Web <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://www.zeldman.com/" target="_blank">Zeldman <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://webfieldmanual.com/" target="_blank">Web Field Manual <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="http://mor10.com" target="_blank">Mor 10 <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://thenextweb.com/" target="_blank">The Next Web <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://www.zeldman.com/" target="_blank">Zeldman <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://webfieldmanual.com/" target="_blank">Web Field Manual <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="http://mor10.com" target="_blank">Mor 10 <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
 
     <div class="col-lg-3 col-md-6 col-sm-6">
       <p>AWARDS</p>
       <ul class="no-bullet">
-        <li><a href="https://cssdesignawards.com/" target="_blank">CSS Design Awards <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://www.webbyawards.com/" target="_blank">Webby Awards <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://www.awwwards.com/" target="_blank">AWW <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://cssdesignawards.com/" target="_blank">CSS Design Awards <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://www.webbyawards.com/" target="_blank">Webby Awards <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://www.awwwards.com/" target="_blank">AWW <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
 
     <div class="col-lg-3 col-md-6 col-sm-6">
       <p>MATERIAL DESIGN</p>
       <ul class="no-bullet">
-        <li><a href="https://m3.material.io/" target="_blank">Google Material Design <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://mdbootstrap.com/docs/standard/" target="_blank">Bootstrap Material Design <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://m3.material.io/" target="_blank">Google Material Design <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://mdbootstrap.com/docs/standard/" target="_blank">Bootstrap Material Design <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
 
     <div class="col-lg-3 col-md-6 col-sm-12 ">
       <p>WEBSITES</p>
       <ul class="no-bullet">
-        <li><a href="https://mikeinghamdesign.com/" target="_blank">Mike Ingham Design <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://skylinedesign.co.ke/" target="_blank">Skyline Design <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://www.gravitatedesign.com/" target="_blank">Gravitate Design <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://michaelkamuiru.info/" target="_blank">Michael Kamuiru <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://www.oho.com/" target="_blank">OHO Interactive <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://expo.getbootstrap.com/" target="_blank">Bootstrap Expo <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://www.kevinleary.net/" target="_blank">Kevin Leary <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://gravita.co" target="_blank">Gravita <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://creativitykills.co/" target="_blank">Creativity Skills <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://mikeinghamdesign.com/" target="_blank">Mike Ingham Design <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://skylinedesign.co.ke/" target="_blank">Skyline Design <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://www.gravitatedesign.com/" target="_blank">Gravitate Design <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://michaelkamuiru.info/" target="_blank">Michael Kamuiru <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://www.oho.com/" target="_blank">OHO Interactive <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://expo.getbootstrap.com/" target="_blank">Bootstrap Expo <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://www.kevinleary.net/" target="_blank">Kevin Leary <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://gravita.co" target="_blank">Gravita <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://creativitykills.co/" target="_blank">Creativity Skills <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
   </section>
@@ -93,34 +93,34 @@
     <div class="col-lg-3 col-md-6 col-sm-12 ">
         <p>CODE</p>
         <ul class="no-bullet">
-          <li><a href="https://css-tricks.com/" target="_blank">CSS Tricks <i class="fa-solid fa-link"></i></a></li>
-          <li><a href="http://emmet.io/" target="_blank">Emmet <i class="fa-solid fa-link"></i></a></li>
-          <li><a href="https://devhints.io/" target="_blank">Cheat Sheets <i class="fa-solid fa-link"></i></a></li>
-          <li><a href="https://getwaves.io">Get Waves <i class="fa-solid fa-link"></i></a></li>
+          <li class="zelinks"><a href="https://css-tricks.com/" target="_blank">CSS Tricks <i class="fa-solid fa-link"></i></a></li>
+          <li class="zelinks"><a href="http://emmet.io/" target="_blank">Emmet <i class="fa-solid fa-link"></i></a></li>
+          <li class="zelinks"><a href="https://devhints.io/" target="_blank">Cheat Sheets <i class="fa-solid fa-link"></i></a></li>
+          <li class="zelinks"><a href="https://getwaves.io">Get Waves <i class="fa-solid fa-link"></i></a></li>
         </ul>
     </div>
 
     <div class="col-lg-3 col-md-6 col-sm-6">
       <p>ICONS</p>
       <ul class="no-bullet">
-        <li><a href="https://fontawesome.com/" target="_blank">Fontawesome <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://fontawesome.com/" target="_blank">Fontawesome <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
 
     <div class="col-lg-3 col-md-6 col-sm-6">
       <p>TESTING RESPONSIVE DESIGNS</p>
       <ul class="no-bullet">
-        <li><a href="http://browserstack.com" target="_blank">Browser Stack <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="http://browserstack.com" target="_blank">Browser Stack <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
 
     <div class="col-lg-3 col-md-6 col-sm-6">
       <p>GOOD FLUID GRID FRAMEWORKS</p>
       <ul class="no-bullet">
-        <li><a href="https://www.bairesdev.com/tools/responsive-grid/" target="_blank">Responsive Grid System <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://html5boilerplate.com/" target="_blank">HTML5 Boilerplate <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="http://getskeleton.com/" target="_blank">Get Skeleton <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="http://responsive-nav.com" target="_blank">Responsive Navigation <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://www.bairesdev.com/tools/responsive-grid/" target="_blank">Responsive Grid System <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://html5boilerplate.com/" target="_blank">HTML5 Boilerplate <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="http://getskeleton.com/" target="_blank">Get Skeleton <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="http://responsive-nav.com" target="_blank">Responsive Navigation <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
   </section>
@@ -129,24 +129,24 @@
     <div class="col-lg-6 col-md-6 col-sm-6">
       <p>WORDPRESS</p>
       <ul class="no-bullet">
-        <li><a href="https://woocommerce.com/documentation/woocommerce/" target="_blank">Woo Themes Docs <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://underscores.me" target="_blank">Underscores <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://roots.io" target="_blank">Roots IO <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://jointswp.com" target="_blank">Joints WP <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://themble.com/bones" target="_blank">Themble Bones <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://themify.me/themes/basic" target="_blank">Themify <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://understrap.com" target="_blank">Understrap <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://woocommerce.com/documentation/woocommerce/" target="_blank">Woo Themes Docs <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://underscores.me" target="_blank">Underscores <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://roots.io" target="_blank">Roots IO <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://jointswp.com" target="_blank">Joints WP <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://themble.com/bones" target="_blank">Themble Bones <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://themify.me/themes/basic" target="_blank">Themify <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://understrap.com" target="_blank">Understrap <i class="fa-solid fa-link"></i></a></li>
       </ul>
     </div>
 
     <div class="col-lg-6 col-md-6 col-sm-6">
       <p>STOCK PHOTOGRAPHY FREE SITES</p>
       <ul class="no-bullet">
-        <li><a href="https://picsum.photos/" target="_blank">Picsum Photos <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://gratisography.com" target="_blank">Gratisography <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://nos.twnsnd.co" target="_blank">New Old Stock <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://getrefe.tumblr.com" target="_blank">Free Refe Mobile Photos <i class="fa-solid fa-link"></i></a></li>
-        <li><a href="https://www.manypixels.co/gallery" target="_blank">Many Pixels <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://picsum.photos/" target="_blank">Picsum Photos <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://gratisography.com" target="_blank">Gratisography <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://nos.twnsnd.co" target="_blank">New Old Stock <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://getrefe.tumblr.com" target="_blank">Free Refe Mobile Photos <i class="fa-solid fa-link"></i></a></li>
+        <li class="zelinks"><a href="https://www.manypixels.co/gallery" target="_blank">Many Pixels <i class="fa-solid fa-link"></i></a></li>
 
       </ul>
     </div>
