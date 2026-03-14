c95de407cc9c51da0e084cffff6f23d23c251e8e
JamesVanWaza <jgacuca@gmail.com>
2026-03-14 12:26:57 -0400

    Created changelog page and updated styling for a buttons on the index page

---
+++ changelog.php
@@ -0,0 +1,10 @@
+<?php include_once 'nav-admin.php';?>
+    
+    <section>
+        <h1>Changelog</h1>
+        <h2>Version 2026.03.09</h2>
+        <h3>March 14, 2026 12:25</h3>
+        <p>The a buttons in  the index page, the navigation is underline on hover.</p>
+    </section>
+
+<?php include 'footer.php';?>
