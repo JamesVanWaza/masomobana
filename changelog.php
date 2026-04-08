92323cb314f8fb6736dfc08e4fff5ce9015e4ca1
JamesVanWaza <jgacuca@gmail.com>
2026-04-08 12:24:04 -0400

    Updated Dependencies

--- changelog.php
+++ changelog.php
@@ -1,10 +1,17 @@
 <?php include_once 'nav-admin.php';?>
     
-    <section>
+    <article>
         <h1>Changelog</h1>
         <h2>Version 2026.03.09</h2>
         <h3>March 14, 2026 12:25</h3>
         <p>The a buttons in  the index page, the navigation is underline on hover.</p>
-    </section>
+    </article>
+
+    <article>
+        <h1>Changelog</h1>
+        <h2>Version 2026.04.10</h2>
+        <h3>April 08, 2026 12:23</h3>
+        <p>Updated dependencies</p>
+    </article>
 
 <?php include 'footer.php';?>
